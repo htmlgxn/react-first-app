d2771a1fcc634d1da5979602b5e8b0065da22b90
Ben Chitty <htmlgxn@protonmail.com>
2021-05-07 16:42:49 -0300

    JSX vs HTML

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return(
-        <div>Hi there!</div>
+        <div>
+            <label for="name" className="label">Enter e-mail: </label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'red', color: 'white' }}>Submit</button>
+        </div>
     )
 }
 
