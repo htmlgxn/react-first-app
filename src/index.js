fb8b27c292b2f61d640c6da517023fdff8caa0cf
Ben Chitty <htmlgxn@protonmail.com>
2021-05-07 17:08:31 -0300

    JSX and objects

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+    const buttonText = {text: 'Hello World'};
+    const style = {backgroundColor: 'red', color: 'white'};
+
     return(
         <div>
             <label for="name" className="label">Enter e-mail: </label>
             <input id="name" type="text"/>
-            <button style={{ backgroundColor: 'red', color: 'white' }}>Submit</button>
+            <button style={style}>{ buttonText.text }</button>
         </div>
     )
 }
